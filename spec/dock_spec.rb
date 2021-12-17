42be09118607875a86337a7fcedfdaae9c78e1c6
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-17 17:05:51 -0600

    Add #rent and :rental_log to track Boats and Renters in Hash

--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -7,6 +7,63 @@ RSpec.describe Dock do
   it 'exists' do
     dock = Dock.new("The Rowing Dock", 3)
 
-    expect(dock).to be_instance_of Dock 
+    expect(dock).to be_instance_of Dock
   end
+
+  it 'instantiates with a name' do
+    dock = Dock.new("The Rowing Dock", 3)
+
+    expect(dock.name).to eq("The Rowing Dock")
+  end
+
+  it 'instantiates with a maximum rental time' do
+    dock = Dock.new("The Rowing Dock", 3)
+
+    expect(dock.max_rental_time).to eq(3)
+  end
+
+  it 'can rent a Boat to a Renter' do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+
+    dock.rent(kayak_1, patrick)
+
+    expect(dock.rental_log.keys).to eq([kayak_1])
+    expect(dock.rental_log.values).to eq([patrick])
+  end
+
+  it 'can rent multiple Boats to one Renter' do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+
+    expect(dock.rental_log.keys).to eq([kayak_1, kayak_2])
+    expect(dock.rental_log.values).to eq([patrick, patrick])
+  end
+
+  it 'can rent multiple Boats to multiple Renters' do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+
+    expected = {
+   kayak_1=> patrick,
+   kayak_2=> patrick,
+   sup_1=> eugene}
+
+   expect(dock.rental_log).to eq(expected)
+  end
+
 end
--- spec/renter_spec.rb
+++ spec/renter_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Renter do
 
     expect(renter).to be_instance_of(Renter)
   end
+
+  it 'instantiates with a name' do
+    renter = Renter.new("Patrick Star", "4242424242424242")
+
+    expect(renter.name).to eq("Patrick Star")
+  end
+
+  it 'instantiates with a credit card number' do
+    renter = Renter.new("Patrick Star", "4242424242424242")
+
+    expect(renter.credit_card_number).to eq("4242424242424242")
+  end
 end
