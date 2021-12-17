97959c4dadec871995132c8580a8edb1fc03b4de
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-17 16:44:06 -0600

    Add Renter class. Initialize with name and credit card number as args

--- lib/renter.rb
+++ lib/renter.rb
@@ -1,6 +1,10 @@
 class Renter
 
-  def initialize
-  end 
+  attr_reader :name, :credit_card_number
+
+  def initialize(name, credit_card_number)
+    @name = name
+    @credit_card_number = credit_card_number
+  end
 
 end
--- spec/renter_spec.rb
+++ spec/renter_spec.rb
@@ -4,5 +4,8 @@ require './lib/boat'
 
 RSpec.describe Renter do
   it 'exists' do
+    renter = Renter.new("Patrick Star", "4242424242424242")
+
+    expect(renter).to be_instance_of(Renter)
   end
 end
